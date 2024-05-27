28edb0ac34951b305fae04e694a9540ce8abdbe4
M.Zeeshan <109668276+M-ZeeshanDev@users.noreply.github.com>
2024-05-27 12:28:13 +0500

    Initial Commit
    Initial Commit

---
+++ DigitalWallet-main/src/DigitalWallet/Features/MultiCurrency/UpdateRatio/Endpoint.cs
@@ -0,0 +1,22 @@
+﻿using Carter;
+
+namespace DigitalWallet.Features.MultiCurrency.UpdateRatio;
+
+public class Endpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app
+            .MapGroup(FeatureManager.Prefix)
+            .WithTags(FeatureManager.EndpointTagName)
+            .MapPatch("/{currency_id:guid:required}",
+            async ([FromBody] UpdateRatioRequest request, [FromRoute(Name = "currency_id")] Guid id,
+                CurrencyService service, CancellationToken cancellationToken) =>
+            {
+                var currencyId = CurrencyId.Create(id);
+                await service.UpdateRationAsync(currencyId, request.Ratio, cancellationToken);
+
+                return Results.Ok("Currency ratio updated successfully!");
+            }).Validator<UpdateRatioRequest>();
+    }
+}
